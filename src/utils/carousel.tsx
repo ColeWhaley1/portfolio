93a673202491034895be7771a277bb71da3f1f6c
ColeWhaley1 <colewhaley1@gmail.com>
2024-10-03 17:14:14 -0400

    get carousel arrow functionality working

--- src/utils/carousel.tsx
+++ src/utils/carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ReactNode } from 'react';
+import React, { useState, ReactNode, useEffect } from 'react';
 import LeftArrow from "../assets/left_arrow.svg";
 import RightArrow from "../assets/right_arrow.svg";
 
@@ -11,31 +11,48 @@ const Carousel: React.FC<CarouselProps> = ({ children, startIndex=0 }) => {
 
     const [currentIndex, setCurrentIndex] = useState(startIndex);
 
-    function lockArrow(id: 'left' | 'right') {
-        console.log(id);
-        // document.getElementById(`${id}-arrow`)?
-
+    function removeArrow(id: 'left' | 'right') {
+        document.getElementById(`${id}-arrow`)?.classList.add("hidden");
     }
 
-    const updateCarousel = (direction: 'next' | 'prev') => {
-        if (direction === 'next') {
-            setCurrentIndex((prevIndex) => {
-                const numChildren: number = React.Children.count(children);
+    function makeArrowVisible(id: 'left' | 'right') {
+        document.getElementById(`${id}-arrow`)?.classList.remove("hidden");
+    }
 
-                const isLastIndex: boolean = prevIndex + 1 >= numChildren;
-                
-                if(isLastIndex){
-                    lockArrow('right');
+    useEffect(() => {
+        const numChildren: number = React.Children.count(children);
+        if(startIndex == 0){
+            removeArrow("left");
+        }
+        if(startIndex == numChildren - 1){
+            removeArrow("right");
+        }
+    }, []);
+
+    const updateCarousel = (direction: 'left' | 'right') => {
+        const numChildren: number = React.Children.count(children);
+        if (direction === 'right') {
+            setCurrentIndex((prevIndex) => {
+
+                const willBeLastIndex: boolean = prevIndex + 1 == numChildren - 1;
+
+                if(willBeLastIndex){
+                    removeArrow('right');
                 }
 
-                return isLastIndex ? prevIndex : prevIndex + 1;
+                makeArrowVisible('left');
+
+                return prevIndex + 1 == numChildren ? prevIndex : prevIndex + 1;
             });
         } else {
             setCurrentIndex((prevIndex) => {
-                if(prevIndex < 0){
-                    lockArrow('left');
+                if(prevIndex - 1 == 0){
+                    removeArrow('left');
                 }
-                return prevIndex < 0 ? prevIndex : prevIndex - 1;
+
+                makeArrowVisible('right');
+
+                return prevIndex - 1 < 0 ? prevIndex : prevIndex - 1;
             });
         }
     };
@@ -56,15 +73,15 @@ const Carousel: React.FC<CarouselProps> = ({ children, startIndex=0 }) => {
             </div>
             <button
                 id="left-arrow"
-                onClick={() => updateCarousel('prev')}
-                className="absolute top-1/2 transform -translate-y-1/2 bg-gray-300 p-2 rounded-full"
+                onClick={() => updateCarousel('left')}
+                className="absolute top-1/2 transform -translate-y-1/2 bg-gray-400 p-2 rounded-full"
             >
                 <img src={LeftArrow} alt="previous slide button" />
             </button>
             <button
                 id="right-arrow"
-                onClick={() => updateCarousel('next')}
-                className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-300 p-2 rounded-full"
+                onClick={() => updateCarousel('right')}
+                className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-400 p-2 rounded-full"
             >
                 <img src={RightArrow} alt="next slide button" />
             </button>
