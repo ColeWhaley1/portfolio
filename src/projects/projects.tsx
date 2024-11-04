e5de6d891c74824e82dbca60849697da070a7519
ColeWhaley1 <colewhaley1@gmail.com>
2024-11-03 21:05:50 -0500

    add slide for hackathon project, need to import video now

--- src/projects/projects.tsx
+++ src/projects/projects.tsx
@@ -42,6 +42,47 @@ const Projects = () => {
                     </div>
                 </div>
             </div>
+
+            {/* slide 2 hackathon */}
+            <div className="relative flex flex-col space-x-0 md:space-x-4 justify-center">
+                <div
+                    className="absolute inset-0 bg-cover bg-center rounded-lg"
+                    style={{ backgroundImage: `url(${AppPreviewBg})`, opacity: 0.5}}
+                />
+
+                <div className="relative z-10 flex w-full items-center justify-center">
+                    <div className="m-6 md:m-12">
+                        <video 
+                            src={OutWorkPreview} 
+                            width="300" 
+                            autoPlay 
+                            muted 
+                            loop={true} 
+                            className="rounded-3xl outline outline-4 outline-black"
+                        />
+                    </div>
+
+                    <div className="p-6 md:p-12 rounded-lg bg-white bg-opacity-70 dark:bg-opacity-50 backdrop-blur-lg flex items-center shadow-lg">
+
+                        <div className="text-black text-lg md:text-2xl space-y-4">
+                            <div className="flex flex-col">
+                                <div className="font-bold">AI-Powered Portfolio Creator 📈</div>
+                                <div className="opacity-70 text-sm">Hackathon Project - Capital One (Finance) Category</div>
+                                <div className="opacity-70 text-sm">1st place 🏆</div>
+                            </div>
+                            <div className="flex space-x-2 text-sm">
+                                <Skill>React</Skill>
+                                <Skill>Typescript</Skill>
+                                <Skill>Python</Skill>
+                                <Skill>Flask</Skill>
+                            </div>
+                            <p className="text-sm md:text-base max-w-96">
+                                How many people can really figure out what stocks to invest in? Let alone, tell you what a stock is? It doesn't matter because this tool utilizes gemini to transform plain, human text into a portfolio of stocks with a corresponding amount to buy.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             
             {/* slide 2 Dessert Oasis */}
             <div className="relative flex flex-col space-x-0 md:space-x-4 justify-center">
